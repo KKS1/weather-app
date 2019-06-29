506878d9f3bca60e97df9f0ab714815240387f18
Minkle Gulati <mink@Minkles-Air.home>
2019-06-29 01:54:16 -0700

    adding body parser

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, resp) => {
   if (req.error || resp.error) {
@@ -14,6 +15,11 @@ app.get('/', (req, resp) => {
   } else {
     resp.render('index');
   }
+});
+
+app.post('/', (req, resp) => {
+  resp.render('index');
+  console.log(req.body.city);
 });
 
 app.listen(port, () => {
